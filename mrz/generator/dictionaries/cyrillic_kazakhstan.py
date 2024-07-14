3f5afda47b687429d995b91bc76605837aa0302a
tolikfilipinskiy <111506428+tolikfilipinskiy@users.noreply.github.com>
2024-07-14 16:54:18 +0300

    Added vietnamese and kazakhstan dict

---
+++ mrz/generator/dictionaries/cyrillic_kazakhstan.py
@@ -0,0 +1,81 @@
+# -*- coding: UTF-8 -*-
+
+# GNU General Public License v3.0
+#
+# Permissions of this strong copyleft license are conditioned on making available
+# complete source code of licensed works and modifications, which include larger works
+# using a licensed work, under the same license. Copyright and license notices must be
+# preserved. Contributors provide an express grant of patent rights.
+#
+# For more information on this, and how to apply and follow the GNU GPL, see:
+# http://www.gnu.org/licenses
+#
+# (ɔ) Iván Rincón 2018
+
+
+# kazakhstan transliteration
+
+# Cyrillic with kazahstan special chars added
+
+transliteration = {
+    u"\u0027": "",                         # '
+    u"\u041C": "M",    u"\u043C": "m",     # М, м
+    u"\u041D": "N",    u"\u043D": "n",     # Н, н
+    u"\u041F": "P",    u"\u043F": "p",     # П, п
+    u"\u0420": "R",    u"\u0440": "r",     # Р, р
+    u"\u0422": "T",    u"\u0442": "t",     # Т, т
+    u"\u042B": "Y",    u"\u044B": "y",     # Ы, ы
+    u"\u0427": "CH",   u"\u0447": "ch",    # Ч, ч
+    u"\u042F": "IA",   u"\u044F": "ia",    # Я, я
+    u"\u042E": "IU",   u"\u044E": "iu",    # Ю, ю
+    u"\u0425": "KH",   u"\u0445": "kh",    # Х, х
+    u"\u0428": "SH",   u"\u0448": "sh",    # Ш, ш
+    u"\u0429": "SHCH", u"\u0449": "shch",  # Щ, щ
+    u"\u0426": "TS",   u"\u0446": "ts",    # Ц, ц
+    u"\u0490": "G",    u"\u0491": "g",     # Ґ, ґ
+    u"\u040E": "U",    u"\u045E": "u",     # Ў, ў
+    u"\u046A": "U",    u"\u046B": "u",     # Ѫ, ѫ
+    u"\u0402": "D",    u"\u0452": "d",     # Ђ, ђ
+    u"\u0405": "DZ",   u"\u0455": "dz",    # Ѕ, ѕ
+    u"\u0408": "J",    u"\u0458": "j",     # Ј, ј
+    u"\u0409": "LJ",   u"\u0459": "lj",    # Љ, љ
+    u"\u040A": "NJ",   u"\u045A": "nj",    # Њ, њ
+    u"\u04BA": "C",    u"\u04BB": "c",     # Һ, һ
+    u"\u040F": "DZ",   u"\u045F": "dz",    # Џ, џ
+    u"\u0404": "IE",   u"\u0454": "ie",    # Є, є
+    u"\u0407": "I",    u"\u0457": "i",     # Ї, ї
+    u"\u0403": "G",    u"\u0453": "g",     # Ѓ, ѓ
+    u"\u0406": "I",    u"\u0456": "i",     # І, і
+    u"\u0410": "A",   u"\u0430": "a",     # А, а
+    u"\u04D8": "A",   u"\u04D9": "a",     # Ә, ә
+    u"\u0411": "B",   u"\u0431": "b",     # Б, б
+    u"\u0414": "D",   u"\u0434": "d",     # Д, д
+    u"\u0401": "E",   u"\u0451": "e",     # Ё, ё
+    u"\u0415": "E",   u"\u0435": "e",     # Е, е
+    u"\u042D": "E",   u"\u044D": "e",     # Э, э
+    u"\u0424": "F",   u"\u0444": "f",     # Ф, ф
+    u"\u0413": "G",   u"\u0433": "g",     # Г, г
+    u"\u0413": "G",   u"\u0433": "g",     # Ғ, ғ
+    u"\u0492": "G",   u"\u0493": "g",     # Ғ, ғ
+    u"\u0418": "I",   u"\u0438": "i",     # И, и
+    u"\u0419": "I",   u"\u0439": "i",     # Й, й
+    u"\u041A": "K",   u"\u043A": "k",     # К, к
+    u"\u049A": "K",   u"\u049B": "k",     # Қ, қ
+    u"\u049C": "K",   u"\u049D": "k",     # Ӄ, ӄ
+    u"\u041B": "L",   u"\u043B": "l",     # Л, л
+    u"\u04A2": "N",   u"\u04A3": "n",     # Ң, ң
+    u"\u004F": "O",   u"\u006F": "o",    # O, o
+    u"\u041E": "O",   u"\u043E": "o",     # О, о
+    u"\u04E8": "O",   u"\u04E9": "o",     # Ө, ө
+    u"\u0421": "S",   u"\u0441": "s",     # С, с
+    u"\u0423": "U",   u"\u0443": "u",     # У, у
+    u"\u04B0": "U",   u"\u04B1": "u",     # Ұ, ұ
+    u"\u04AE": "U",   u"\u04AF": "u",     # Ү, ү
+    u"\u0412": "V",   u"\u0432": "v",     # В, в
+    u"\u0417": "Z",   u"\u0437": "z",     # З, з
+    u"\u0416": "ZH",  u"\u0436": "zh",    # Ж, ж
+    u"\u0496": "ZH",  u"\u0497": "zh"     # Җ, җ
+}
+
+# https://www.icao.int/publications/Documents/9303_p3_cons_en.pdf
+# https://en.wikipedia.org/wiki/Cyrillic_script_in_Unicode
---
+++ mrz/generator/dictionaries/vietnamese.py
@@ -0,0 +1,99 @@
+# -*- coding: UTF-8 -*-
+
+# Transliteration vietnamese
+
+transliteration = {
+    u"\u0041": "A",   u"\u0061": "a",    # A, a
+    u"\u0102": "A",   u"\u0103": "a",    # Ă, ă
+    u"\u00C2": "A",   u"\u00E2": "a",    # Â, â
+    u"\u0042": "B",   u"\u0062": "b",    # B, b
+    u"\u0043": "C",   u"\u0063": "c",    # C, c
+    u"\u0044": "D",   u"\u0064": "d",    # D, d
+    u"\u0110": "D",   u"\u0111": "d",    # Đ, đ
+    u"\u0045": "E",   u"\u0065": "e",    # E, e
+    u"\u00CA": "E",   u"\u00EA": "e",    # Ê, ê
+    u"\u0047": "G",   u"\u0067": "g",    # G, g
+    u"\u0048": "H",   u"\u0068": "h",    # H, h
+    u"\u0049": "I",   u"\u0069": "i",    # I, i
+    u"\u004B": "K",   u"\u006B": "k",    # K, k
+    u"\u004C": "L",   u"\u006C": "l",    # L, l
+    u"\u004D": "M",   u"\u006D": "m",    # M, m
+    u"\u004E": "N",   u"\u006E": "n",    # N, n
+    u"\u004F": "O",   u"\u006F": "o",    # O, o
+    u"\u00D4": "O",   u"\u00F4": "o",    # Ô, ô
+    u"\u01A0": "O",   u"\u01A1": "o",    # Ơ, ơ
+    u"\u0050": "P",   u"\u0070": "p",    # P, p
+    u"\u0051": "Q",   u"\u0071": "q",    # Q, q
+    u"\u0052": "R",   u"\u0072": "r",    # R, r
+    u"\u0053": "S",   u"\u0073": "s",    # S, s
+    u"\u0054": "T",   u"\u0074": "t",    # T, t
+    u"\u0055": "U",   u"\u0075": "u",    # U, u
+    u"\u01AF": "U",   u"\u01B0": "u",    # Ư, ư
+    u"\u0056": "V",   u"\u0076": "v",    # V, v
+    u"\u0058": "X",   u"\u0078": "x",    # X, x
+    u"\u0059": "Y",   u"\u0079": "y",    # Y, y
+    u"\u00C1": "A",   u"\u00E1": "a",    # Á, á
+    u"\u00C0": "A",   u"\u00E0": "a",    # À, à
+    u"\u1EA2": "A",   u"\u1EA3": "a",    # Ả, ả
+    u"\u00C3": "A",   u"\u00E3": "a",    # Ã, ã
+    u"\u1EA0": "A",   u"\u1EA1": "a",    # Ạ, ạ
+    u"\u1EAE": "A",   u"\u1EAF": "a",    # Ắ, ắ
+    u"\u1EB0": "A",   u"\u1EB1": "a",    # Ằ, ằ
+    u"\u1EB2": "A",   u"\u1EB3": "a",    # Ẳ, ẳ
+    u"\u1EB4": "A",   u"\u1EB5": "a",    # Ẵ, ẵ
+    u"\u1EB6": "A",   u"\u1EB7": "a",    # Ặ, ặ
+    u"\u1EA4": "A",   u"\u1EA5": "a",    # Ấ, ấ
+    u"\u1EA6": "A",   u"\u1EA7": "a",    # Ầ, ầ
+    u"\u1EA8": "A",   u"\u1EA9": "a",    # Ẩ, ẩ
+    u"\u1EAA": "A",   u"\u1EAB": "a",    # Ẫ, ẫ
+    u"\u1EAC": "A",   u"\u1EAD": "a",    # Ậ, ậ
+    u"\u00C9": "E",   u"\u00E9": "e",    # É, é
+    u"\u00C8": "E",   u"\u00E8": "e",    # È, è
+    u"\u1EBA": "E",   u"\u1EBB": "e",    # Ẻ, ẻ
+    u"\u1EBC": "E",   u"\u1EBD": "e",    # Ẽ, ẽ
+    u"\u1EB8": "E",   u"\u1EB9": "e",    # Ẹ, ẹ
+    u"\u1EBE": "E",   u"\u1EBF": "e",    # Ế, ế
+    u"\u1EC0": "E",   u"\u1EC1": "e",    # Ề, ề
+    u"\u1EC2": "E",   u"\u1EC3": "e",    # Ể, ể
+    u"\u1EC4": "E",   u"\u1EC5": "e",    # Ễ, ễ
+    u"\u1EC6": "E",   u"\u1EC7": "e",    # Ệ, ệ
+    u"\u00CD": "I",   u"\u00ED": "i",    # Í, í
+    u"\u00CC": "I",   u"\u00EC": "i",    # Ì, ì
+    u"\u1EC8": "I",   u"\u1EC9": "i",    # Ỉ, ỉ
+    u"\u0128": "I",   u"\u0129": "i",    # Ĩ, ĩ
+    u"\u1ECA": "I",   u"\u1ECB": "i",    # Ị, ị
+    u"\u00D3": "O",   u"\u00F3": "o",    # Ó, ó
+    u"\u00D2": "O",   u"\u00F2": "o",    # Ò, ò
+    u"\u1ECE": "O",   u"\u1ECF": "o",    # Ỏ, ỏ
+    u"\u00D5": "O",   u"\u00F5": "o",    # Õ, õ
+    u"\u1ECC": "O",   u"\u1ECD": "o",    # Ọ, ọ
+    u"\u1ED0": "O",   u"\u1ED1": "o",    # Ố, ố
+    u"\u1ED2": "O",   u"\u1ED3": "o",    # Ồ, ồ
+    u"\u1ED4": "O",   u"\u1ED5": "o",    # Ổ, ổ
+    u"\u1ED6": "O",   u"\u1ED7": "o",    # Ỗ, ỗ
+    u"\u1ED8": "O",   u"\u1ED9": "o",    # Ộ, ộ
+    u"\u1EDA": "O",   u"\u1EDB": "o",    # Ớ, ớ
+    u"\u1EDC": "O",   u"\u1EDD": "o",    # Ờ, ờ
+    u"\u1EDE": "O",   u"\u1EDF": "o",    # Ở, ở
+    u"\u1EE0": "O",   u"\u1EE1": "o",    # Ỡ, ỡ
+    u"\u1EE2": "O",   u"\u1EE3": "o",    # Ợ, ợ
+    u"\u00DA": "U",   u"\u00FA": "u",    # Ú, ú
+    u"\u00D9": "U",   u"\u00F9": "u",    # Ù, ù
+    u"\u1EE6": "U",   u"\u1EE7": "u",    # Ủ, ủ
+    u"\u0168": "U",   u"\u0169": "u",    # Ũ, ũ
+    u"\u1EE4": "U",   u"\u1EE5": "u",    # Ụ, ụ
+    u"\u1EE8": "U",   u"\u1EE9": "u",    # Ứ, ứ
+    u"\u1EEA": "U",   u"\u1EEB": "u",    # Ừ, ừ
+    u"\u1EEC": "U",   u"\u1EED": "u",    # Ử, ử
+    u"\u1EEE": "U",   u"\u1EEF": "u",    # Ữ, ữ
+    u"\u1EF0": "U",   u"\u1EF1": "u",    # Ự, ự
+    u"\u00DD": "Y",   u"\u00FD": "y",    # Ý, ý
+    u"\u1EF2": "Y",   u"\u1EF3": "y",    # Ỳ, ỳ
+    u"\u1EF6": "Y",   u"\u1EF7": "y",    # Ỷ, ỷ
+    u"\u1EF8": "Y",   u"\u1EF9": "y",    # Ỹ, ỹ
+    u"\u1EF4": "Y",   u"\u1EF5": "y"     # Ỵ, ỵ
+}
+
+# Ссылки на правила транслитерации:
+# https://www.icao.int/publications/Documents/9303_p3_cons_en.pdf
+# https://en.wikipedia.org/wiki/List_of_Unicode_characters
